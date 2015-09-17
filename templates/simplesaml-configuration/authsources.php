2d5fbbf59a5b38967537499ce9e204004f232895
matt.peel <matt@silverstripe.com>
2015-09-18 11:37:35 +1200

    Add template authsources.php and saml20-idp-remote.php files for SimpleSAMLphp

---
+++ templates/simplesaml-configuration/authsources.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * SAML 2.0 Authentication Source list for SimpleSAMLphp.
+ *
+ * This is a template file, which is processed by `RealMeSetupTask` and dropped into the SimpleSAMLphp configuration
+ * directory. This file should not be directed edited once it is in place within the SimpleSAMLphp codebase, instead the
+ * template file should be edited, and then the RealMeSetupTask re-run with force=1 set to override existing files.
+ *
+ * This template is designed to be used within the `RealMeSetupTask` code, and has {{variables}} scattered throughout to
+ * add custom configuration where required.
+ *
+ * To see all variables that can be included here, and documentation around them, check out SimpleSAMLphp's
+ * authsources.php config template here:
+ * https://github.com/simplesamlphp/simplesamlphp/blob/master/config-templates/authsources.php
+ *
+ * @see RealMeSetupTask::run()
+ * @see https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote
+ */
+
+$config = array(
+	// Authentication source which handles admin authentication for accessing /simplesaml/index.php. See config.php
+	'admin' => array(
+		'core:AdminPassword',
+	),
+
+	// MTS - RealMe Messaging Test Site Environment
+	'realme-mts' => array(
+		'saml:SP',
+		'entityID' => '{{mts-entityID}}', // http://dev.realme-integration.govt.nz/onlineservices/service1
+		'idp' => 'https://mts.realme.govt.nz/saml2',
+		'discoURL' => null,
+
+		'NameIDPolicy' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+		'AssertionConsumerServiceURL' => null,
+		'AuthnContextClassRef' => '{{mts-authncontext}}', // e.g. urn:nzl:govt:ict:stds:authn:deployment:GLS:SAML:2.0:ac:classes:LowStrength and ModStrength
+		'ProtocolBinding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
+		'redirect.sign' => true,
+		'ForceAuthn' => false,
+		// @todo SHA1 (the default) is deprecated and old, does RealMe support anything else?
+		// 'signature.algorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
+
+		'privatekey' => 'mts_saml_sp.pem',
+		'privatekey_pass' => 'password',
+		'saml.SOAPClient.certificate' => 'mts_mutual_ssl_sp.pem',
+		'saml.SOAPClient.privatekey_pass' => 'password',
+	),
+
+	// ITE - RealMe Integrated Test Environment
+	'realme-ite' => array(
+		'saml:SP',
+		'entityID' => '{{ite-entityID}}', // https://realme-demo.cwp.govt.nz/realme-demo/service1
+		'idp' => 'https://www.ite.logon.realme.govt.nz/saml2',
+		'discoURL' => null,
+
+		'NameIDPolicy' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+		'AssertionConsumerServiceURL' => null,
+		'AuthnContextClassRef' => '{{ite-authncontext}}', // As above
+		'ProtocolBinding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
+		'redirect.sign' => true,
+		'ForceAuthn' => false,
+		// @todo SHA1 (the default) is deprecated and old, does RealMe support anything else?
+		// 'signature.algorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
+
+		'privatekey' => '{{ite-privatepemfile-signing}}',
+		'saml.SOAPClient.certificate' => '{{ite-privatepemfile-mutual}}',
+	),
+
+	// Production - RealMe Production Environment
+	'realme-prod' => array(
+		'saml:SP',
+		'entityID' => '{{prod-entityID}}',
+		'idp' => 'https://www.logon.realme.govt.nz/saml2',
+		'discoURL' => NULL,
+
+		'NameIDPolicy' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+		'AssertionConsumerServiceURL' => null,
+		'AuthnContextClassRef' => '{{prod-authncontext}}',
+		'ProtocolBinding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Artifact',
+		'redirect.sign' => TRUE,
+		'ForceAuthn' => FALSE,
+		// @todo SHA1 (the default) is deprecated and old, does RealMe support anything else?
+		// 'signature.algorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
+
+		'privatekey' => '{{prod-privatepemfile-signing}}',
+		'saml.SOAPClient.certificate' => '{{prod-privatepemfile-mutual}}',
+	)
+);
---
+++ templates/simplesaml-configuration/saml20-idp-remote.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * SAML 2.0 Remote Identity Provider (IdP) metadata for SimpleSAMLphp.
+ *
+ * This is a template file, which is processed by `RealMeSetupTask` and dropped into the SimpleSAMLphp metadata
+ * configuration directory. This file should not be directed edited once it is in place within the SimpleSAMLphp
+ * codebase, instead the template file should be edited, and then the RealMeSetupTask re-run with force=1 set to
+ * override existing files.
+ *
+ * This template is designed to be used within the `RealMeSetupTask` code, and may have {{variables}} scattered
+ * throughout to add custom configuration where required.
+ *
+ * To see all variables that can be included here, and documentation around them, check out SimpleSAMLphp's reference
+ * information here: https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote
+ *
+ * @see RealMeSetupTask::run()
+ * @see https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote
+ */
+
+// MTS - RealMe Messaging Test Site Environment
+$metadata['https://mts.realme.govt.nz/saml2'] = array(
+	'name' => 'MTS',
+	'description' => 'Real Me MTS authentication system',
+
+	'SingleSignOnService' => 'https://mts.realme.govt.nz/logon-mts/mtsEntryPoint',
+	'SingleSignOnService.artifact' => 'https://mts.realme.govt.nz/logon-mts/mtsEntryPoint',
+
+	'certificate' => 'mts_saml_idp.cer',
+	'saml.SOAPClient.certificate' => 'mts_mutual_ssl_sp.cer',
+	'saml.SOAPClient.privatekey_pass' => 'password',
+
+	'ArtifactResolutionService' => array(
+		array(
+			'index' => 0,
+			'Location' => 'https://as.mts.realme.govt.nz/sso/ArtifactResolver/metaAlias/logon/logonidp',
+			'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP'
+		)
+	)
+);
+
+// ITE - RealMe Integrated Test Environment
+$metadata['https://www.ite.logon.realme.govt.nz/saml2'] = array(
+	'name' => 'ITE',
+	'description' => 'Real Me ITE authentication system',
+
+	'SingleSignOnService' => 'https://www.ite.logon.realme.govt.nz/sso/logon/metaAlias/logon/logonidp',
+	'SingleSignOnService.artifact' => 'https://www.ite.logon.realme.govt.nz/sso/logon/metaAlias/logon/logonidp',
+
+	'certificate' => 'ite.signing.logon.realme.govt.nz.cer',
+	'saml.SOAPClient.certificate' => 'as.ite.logon.realme.govt.nz.cer',
+
+	'ArtifactResolutionService' => array(
+		array(
+			'index' => 0,
+			'Location' => 'https://as.ite.logon.realme.govt.nz/sso/ArtifactResolver/metaAlias/logon/logonidp',
+			'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP'
+		)
+	)
+);
+
+// Production - RealMe Production Environment
+$metadata['https://www.logon.realme.govt.nz/saml2'] = array(
+	'name' => 'Production',
+	'description' => 'Real Me Production authentication system',
+
+	'SingleSignOnService' => 'https://www.logon.realme.govt.nz/sso/logon/metaAlias/logon/logonidp',
+	'SingleSignOnService.artifact' => 'https://www.logon.realme.govt.nz/sso/logon/metaAlias/logon/logonidp',
+
+	'certificate' => 'signing.logon.realme.govt.nz.cer',
+	'saml.SOAPClient.certificate' => 'as.logon.realme.govt.nz.cer',
+
+	'ArtifactResolutionService' => array(
+		array(
+			'index' => 0,
+			'Location' => 'https://as.logon.realme.govt.nz/sso/ArtifactResolver/metaAlias/logon/logonidp',
+			'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:SOAP'
+		)
+	)
+);
