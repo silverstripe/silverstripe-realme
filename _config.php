6d03888da9168f6d5eb8465e6c14f0b08bfa7595
Mike <mike@silverstripe.com>
2015-09-25 11:40:26 +1200

    Add REALME_DIR to config.php for theme requirements

--- _config.php
+++ _config.php
@@ -1,4 +1,5 @@
 <?php
 Authenticator::register_authenticator('RealMeAuthenticator');
 
+// defines the base directory, used by RealMeLoginForm to include javascript and css via Requirements
 define('REALME_MODULE_PATH', basename(dirname(__FILE__)));
